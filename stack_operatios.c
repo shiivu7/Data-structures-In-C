a32742081d4e663869ccf70e3c4b8243535791ad
shiivu7 <shokdarshivum@gmail.com>
2025-10-26 16:48:30 +0530

    pallindrome check using stack

--- stack_operatios.c
+++ stack_operatios.c
@@ -6,20 +6,22 @@ int stack[MAX],item,top=-1,status=0,ch;
 void push()
 {
     if(top==(MAX-1))
-    printf("\nOverflow");
+    printf("\nOverflow\n");
     else
     stack[++top]=item;
     status++;
 }
+
 int pop()
 {
     if(top==-1)
-    printf("\nUnderflow");
+    printf("\nUnderflow\n");
     else
     return stack[top--];
     status--;
 }
-void pallindrome(int Stack[])
+
+void pallindrome(int stack[])
 {
     int temp,count=0;
     temp=status;
@@ -29,46 +31,53 @@ void pallindrome(int Stack[])
         count++;
 
     }
-    if(count==temp)
-    printf("pallindome\n");
+    if (count==temp)
+    printf("\npallindrome\n");
     else
-    printf("not pallidrome\n");
+     printf("\n not a pallindrome\n");
+
 }
 
 void display(int stack[])
 {
-    if(top==-1)
-    printf("\nstack is empty\n");
+    if (top==-1)
+    printf("stack is empty");
     else
-    for(int i=top;i>=0;i--)
+    for(int i=top;i>0;i--)
     {
         printf("|%d|\n",stack[i]);
     }
+
 }
+
 void main()
 {
     int ch;
     while(1)
     {
-        printf("\n1.push\t 2.pop\t 3.pallindrome\t 4.display\t 5.exit\n");
-        printf("enter your choice: ");
+        printf("\n1.push\t2.pop\t3.pallindrome\t4.display\t5.exit\n");
+        printf("Enter your choice");
         scanf("%d",&ch);
         switch(ch)
         {
-            case 1:printf("enter the item:");
+            case 1:printf("\nenter an item\n");
             scanf("%d",&item);
             push(stack,item);
             break;
-            case 2:printf("popped value =%d",pop());
+            case 2:printf("\npopped value= %d",pop());
             break;
             case 3:pallindrome(stack);
             break;
             case 4:display(stack);
             break;
             case 5:exit(0);
-            default:printf("invalid choice\n");
             break;
+            default:printf("\ninvalid input value\n");
+            break;
+
+
         }
-       
     }
 }
+
+
